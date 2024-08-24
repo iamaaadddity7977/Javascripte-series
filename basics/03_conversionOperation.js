b5024a1a09a3ae56848ee1d8d67353653bb8c9e8
Aditya Yadav <108022811+iamaaadddity7977@users.noreply.github.com>
2024-08-24 09:39:27 +0000

    conversion operation

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "38aaa"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+// "38" => 38
+// "38" => NaN
+// true => 1 ; false => 0
+
+
+// hum log yaha pe number ko string ki tarha rakha hai but type of mein nuber hi dikhata hai
+// jaise hi let value liya wow number mein conver hogya NaN 
+
+let isLoggedIn = "aaa"  //0 "" false  true 
+let booleanIsLoggingIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggingIn);
+
+// 1 = true : false = 0
+//  "" = false
+//  "aaaa" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
